e5686fa13cf8e836ec2e6913b23f53a00ca84aa5
vijay <vijayadaran@gmail.com>
2020-06-21 13:38:42 +0530

    finished latest news and client section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-$('.owl-carousel').owlCarousel({
+$('#works-carousel').owlCarousel({
+    // loop:true,
+    margin:10,
+    center:false,
+    nav:false,
+    // navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('#latest-carousel').owlCarousel({
     // loop:true,
     margin:10,
     center:false,
